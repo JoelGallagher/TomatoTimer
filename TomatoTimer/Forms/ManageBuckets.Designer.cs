41ee4fee39d474fbe32ca6b2457eb880538b924e
Joel Gallagher <joel@joelgallagher.com>
2015-07-07 22:42:08 +1000

    Set up dynamic button creation for ManageBuckets. enums for formstate, work in progress

--- TomatoTimer/Forms/ManageBuckets.Designer.cs
+++ TomatoTimer/Forms/ManageBuckets.Designer.cs
@@ -30,48 +30,104 @@
         {
             this.components = new System.ComponentModel.Container();
             this.cmdClose = new System.Windows.Forms.Button();
-            this.gridBuckets = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtBucketName = new System.Windows.Forms.TextBox();
+            this.cmdDelete = new System.Windows.Forms.Button();
+            this.cmdSave = new System.Windows.Forms.Button();
+            this.pnlBuckets = new System.Windows.Forms.FlowLayoutPanel();
             this.bucketBindingSource = new System.Windows.Forms.BindingSource(this.components);
-            ((System.ComponentModel.ISupportInitialize)(this.gridBuckets)).BeginInit();
+            this.btnNewBucket = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.bucketBindingSource)).BeginInit();
             this.SuspendLayout();
             // 
             // cmdClose
             // 
-            this.cmdClose.Location = new System.Drawing.Point(323, 301);
+            this.cmdClose.Location = new System.Drawing.Point(236, 317);
+            this.cmdClose.Margin = new System.Windows.Forms.Padding(4);
             this.cmdClose.Name = "cmdClose";
-            this.cmdClose.Size = new System.Drawing.Size(75, 23);
+            this.cmdClose.Size = new System.Drawing.Size(100, 28);
             this.cmdClose.TabIndex = 0;
             this.cmdClose.Text = "Close";
             this.cmdClose.UseVisualStyleBackColor = true;
             this.cmdClose.Click += new System.EventHandler(this.cmdClose_Click);
             // 
-            // gridBuckets
+            // label1
             // 
-            this.gridBuckets.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.gridBuckets.EditMode = System.Windows.Forms.DataGridViewEditMode.EditOnF2;
-            this.gridBuckets.Location = new System.Drawing.Point(12, 25);
-            this.gridBuckets.Name = "gridBuckets";
-            this.gridBuckets.Size = new System.Drawing.Size(240, 150);
-            this.gridBuckets.TabIndex = 1;
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(16, 235);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(63, 17);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Bucket : ";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // txtBucketName
+            // 
+            this.txtBucketName.Location = new System.Drawing.Point(118, 235);
+            this.txtBucketName.Name = "txtBucketName";
+            this.txtBucketName.Size = new System.Drawing.Size(218, 22);
+            this.txtBucketName.TabIndex = 3;
+            // 
+            // cmdDelete
+            // 
+            this.cmdDelete.Location = new System.Drawing.Point(258, 273);
+            this.cmdDelete.Name = "cmdDelete";
+            this.cmdDelete.Size = new System.Drawing.Size(75, 23);
+            this.cmdDelete.TabIndex = 4;
+            this.cmdDelete.Text = "Delete";
+            this.cmdDelete.UseVisualStyleBackColor = true;
+            // 
+            // cmdSave
+            // 
+            this.cmdSave.Location = new System.Drawing.Point(177, 273);
+            this.cmdSave.Name = "cmdSave";
+            this.cmdSave.Size = new System.Drawing.Size(75, 23);
+            this.cmdSave.TabIndex = 6;
+            this.cmdSave.Text = "Save";
+            this.cmdSave.UseVisualStyleBackColor = true;
+            // 
+            // pnlBuckets
+            // 
+            this.pnlBuckets.AutoScroll = true;
+            this.pnlBuckets.Location = new System.Drawing.Point(19, 12);
+            this.pnlBuckets.Name = "pnlBuckets";
+            this.pnlBuckets.Size = new System.Drawing.Size(317, 167);
+            this.pnlBuckets.TabIndex = 7;
             // 
             // bucketBindingSource
             // 
             this.bucketBindingSource.DataSource = typeof(TomatoTimer.Entities.Bucket);
             // 
+            // btnNewBucket
+            // 
+            this.btnNewBucket.Location = new System.Drawing.Point(19, 185);
+            this.btnNewBucket.Name = "btnNewBucket";
+            this.btnNewBucket.Size = new System.Drawing.Size(75, 23);
+            this.btnNewBucket.TabIndex = 8;
+            this.btnNewBucket.Text = "New";
+            this.btnNewBucket.UseVisualStyleBackColor = true;
+            this.btnNewBucket.Click += new System.EventHandler(this.btnNewBucket_Click);
+            // 
             // ManageBuckets
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(410, 336);
-            this.Controls.Add(this.gridBuckets);
+            this.ClientSize = new System.Drawing.Size(356, 361);
+            this.Controls.Add(this.btnNewBucket);
+            this.Controls.Add(this.pnlBuckets);
+            this.Controls.Add(this.cmdSave);
+            this.Controls.Add(this.cmdDelete);
+            this.Controls.Add(this.txtBucketName);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.cmdClose);
-            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
             this.Name = "ManageBuckets";
             this.Text = "ManageBuckets";
-            ((System.ComponentModel.ISupportInitialize)(this.gridBuckets)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.bucketBindingSource)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -79,7 +135,12 @@
 
         private System.Windows.Forms.BindingSource bucketBindingSource;
         private System.Windows.Forms.Button cmdClose;
-        private System.Windows.Forms.DataGridView gridBuckets;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtBucketName;
+        private System.Windows.Forms.Button cmdDelete;
+        private System.Windows.Forms.Button cmdSave;
+        private System.Windows.Forms.FlowLayoutPanel pnlBuckets;
+        private System.Windows.Forms.Button btnNewBucket;
 
 
     }
--- TomatoTimer/Forms/ManageBuckets.cs
+++ TomatoTimer/Forms/ManageBuckets.cs
@@ -9,6 +9,13 @@ namespace TomatoTimer.Forms
         private ITomatoService _tomatoService;
         private IBucketService _bucketService;
 
+        private enum formState
+        {
+            clean,
+            editingBucket,
+            newBucket
+        }
+
         #region CTOR
 
         public ManageBuckets(ITomatoService tomatoService, IBucketService bucketService)
@@ -30,13 +37,32 @@ namespace TomatoTimer.Forms
         private void LoadBuckets()
         {
             var buckets = _bucketService.GetAll();
+            foreach (var bucket in buckets)
+            {
+                var button = new Button();
+                button.Text = bucket.Name;
+                button.Name = "btn" + bucket.Name;
+                button.Click += btnBucketClick;
+                pnlBuckets.Controls.Add(button);
+            }
+        }
 
-            gridBuckets.DataSource = buckets;
+        private void btnBucketClick(object sender, EventArgs e)
+        {
+            var x = sender;
         }
 
         private void cmdClose_Click(object sender, EventArgs e)
         {
             Close();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void btnNewBucket_Click(object sender, EventArgs e)
+        {
         }
     }
 }
